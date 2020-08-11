2adb0c692000b996e01c289221f466296d5465de
sergx961 <feodor.alexandrov@yandex.ru>
2020-08-12 00:06:00 +0300

    fix merge

--- software/inc/uart_data_transfer/uart_data_transfer_core.h
+++ software/inc/uart_data_transfer/uart_data_transfer_core.h
@@ -1,11 +1,7 @@
 #ifndef _UART_DATA_TRANSFER_CORE_H_
 #define _UART_DATA_TRANSFER_CORE_H_
 
-<<<<<<< HEAD
 // #define DEBUG_SOFTWARE
-=======
-//#define DEBUG_SOFTWARE
->>>>>>> 9a4334a8322bae6c61956904f4ce1337067b2081
 
 void init_uart_data_transfer (void);
 void transmit_data_by_uart (void);
