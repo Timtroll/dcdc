4d57f366ea5933a1a90a3533dfa85f42ea936410
BestRamsay <a.ta.2000@mail.ru>
2020-08-26 17:34:56 +0300

    Added in sys_info function stub functions

--- software/inc/system_info/system_info.h
+++ software/inc/system_info/system_info.h
@@ -15,5 +15,17 @@ typedef struct {
 	action_t action;
 } parser_memory;
 
+char * sys_info_get_voltage_cut(void);
+char * sys_info_get_charging_type(void);
+char * sys_info_get_charge_threshold_Pb_lower(void);
+char * sys_info_get_charge_threshold_Pb_upper(void);
+char * sys_info_get_charge_threshold_others(void);
+char * sys_info_get_discharge_threshold(void);
+char * sys_info_get_quantity_cans(void);
+char * sys_info_get_capacity(void);
+char * sys_info_get_max_allowable_capacity(void);
+char * sys_info_get_internal_voltage_default(void);
+char * sys_info_get_type_battery(void);
+
 #endif//_SYSTEM_INFO_H_
 
--- software/src/system_info/system_info.c
+++ software/src/system_info/system_info.c
@@ -3,3 +3,37 @@
 void save_to_queue(parser_memory struct_to_save){
 	//here will be code, when adress of queue is known
 }
+
+char * sys_info_get_voltage_cut(void){
+	return "voltage_cut";
+}
+char * sys_info_get_charging_type(void){
+	return "charging_type";
+}
+char * sys_info_get_charge_threshold_Pb_lower(void){
+	return "charge_threshold_Pb_lower";
+}
+char * sys_info_get_charge_threshold_Pb_upper(void){
+	return "charge_threshold_Pb_upper";
+}
+char * sys_info_get_charge_threshold_others(void){
+	return "harge_threshold_others";
+}
+char * sys_info_get_discharge_threshold(void){
+	return "discharge_threshold";
+}
+char * sys_info_get_quantity_cans(void){
+	return "quantity_cans";
+}
+char * sys_info_get_capacity(void){
+	return "capacity";
+}
+char * sys_info_get_max_allowable_capacity(void){
+	return "max_allowable_capacity";
+}
+char * sys_info_get_internal_voltage_default(void){
+	return "internal_voltage_default";
+}
+char * sys_info_get_type_battery(void){
+	return "type_battery";
+} 
