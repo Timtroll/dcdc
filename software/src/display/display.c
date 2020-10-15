f285b4dd2fb0183b4c185f942b9ff5bbb5437f50
BestRamsay <a.ta.2000@mail.ru>
2020-10-15 22:19:51 +0300

    In the screen block, functions for switching screens and processing JSON requests were tested

--- software/src/display/display.c
+++ software/src/display/display.c
@@ -1,38 +1,30 @@
 #include "display_internal.h"
 
 void display_start_set_data(void){
-	//permission
 	if (DISABLE == get_permission_status())
-	{
 		change_permission_status();
-	}
 }
 void display_stop_set_data(void){
-	//unpermisson
 	if (ENABLE == get_permission_status())
-	{
 		change_permission_status();
-	}
 }
 
 //All functions require permission to change information in the cell
 //so the user has to allow changes
 void display_get_parameter(char * json_request){
-	if (ENABLE == get_permission_status()){
-		//This function processes the received json string and uses the public methods of 
-		//other blocks to pass the response to the I/O block.
-	}
+	#ifdef _DEBUG_DISPLAY_
+		set_transmit_string(parser_accept_json_string(json_request));
+	#endif
+		//There will be data transfer via UART to I/O
+	//This function processes the received JSON string and uses the public methods of 
+	//other blocks to pass the response to the I/O block.
 }
 void display_set_next_screen(uint8_t direction){
-	if (ENABLE == get_permission_status()){
+	if (DISABLE == get_permission_status()){
 		if (direction == RIGHT)
-		{
 			menu_swipe_right();
-
-		} else
-		{
+		else
 			menu_swipe_left();
-		}
 		//This function selects a new screen in the menu, depending on the selected direction.
 		// Overwrites data inside the display, for return functions
 	}
@@ -42,3 +34,13 @@ void display_set_parameter(char * cell_data){
 		//This function rewrited cell in menu
 	}
 }
+#ifdef _DEBUG_DISPLAY_
+	static char * debug_transmit_string = NULL;
+
+	void set_transmit_string(char * value){
+		debug_transmit_string = value;
+	}
+	char * get_transmit_string(void){
+		return debug_transmit_string;
+	}
+#endif
