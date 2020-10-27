3006cfa5aa4214d05c4c33c4a10131d29a35199a
BestRamsay <a.ta.2000@mail.ru>
2020-10-27 16:59:18 +0300

    fixed dangerously macro and added inline func; fixed atoi function

--- software/src/user_actions/user_actions_internal.c
+++ software/src/user_actions/user_actions_internal.c
@@ -5,28 +5,38 @@
 #define WORD_END 2
 #define PHRASE_END 3
 
-#define Extra_letters(string, cntr) \
-	for ( ; cntr < strlen(string); cntr++) \
-	{ \
-		if (string[cntr] >= 'a' && string[cntr] <= 'z' ) \
-			return INCORRECT_PARAMETER_INPUT; \
-	} \
-	cntr = 0; \
+static inline uint8_t extra_letters(char * string, uint8_t cntr){
+	if (strlen(string) > MAX_LEN) return  INCORRECT_PARAMETER_INPUT;
+	for (; cntr < strlen(string); cntr++)
+	{
+		if (string[cntr] >= 'a' && string[cntr] <= 'z' ) 
+			return INCORRECT_PARAMETER_INPUT; 
+	}
+	return SUCCESSFUL;
+}	
 
 char * _clear_data(char * parameter){
 	if (strlen(parameter) > MAX_LEN)
 			return NULL;
 
-	static char cleared_string [MAX_LEN] = " ";	
-	uint16_t counter = 0;
-	uint16_t internal_counter = 0;
-	uint8_t whole_word = PHRASE_START;
+	static char cleared_string [MAX_LEN] = " ";		
+	uint16_t 
+			counter = 0,
+		 	internal_counter = 0;
+	uint8_t 
+			whole_word = PHRASE_START,
+			return_value = 0;
 
-	if (strcmp(cleared_string, " ") != 0)
+	if (strncmp(cleared_string, " ", MAX_LEN) != 0) 
 		memset(cleared_string, 0, MAX_LEN);
 	char cmp_string [MAX_LEN];
-	strcpy(cmp_string, cleared_string);
-	
+	for (; counter < MAX_LEN; counter++)
+		if (cleared_string[counter] != " ") internal_counter++;
+
+	strncpy(cmp_string, cleared_string, internal_counter);
+	counter = 0;
+	internal_counter = 0;
+
 	while(counter < strlen(parameter))
 	{
 		if (((parameter[counter] == ' ') || (parameter[counter] == '\t'))  && (cleared_string != " " )){
@@ -49,43 +59,50 @@ char * _clear_data(char * parameter){
 		}else if (cleared_string[0] !=  ' ' )
 			return NULL;	
 	}
-	if (strcmp(cleared_string, cmp_string) ==  0)
+	if (strncmp(cleared_string, cmp_string, MAX_LEN) ==  0)
 		return NULL;
 	return cleared_string;
 }
 
 uint8_t check_parameter(uint8_t type_parameter, char * parameter){
 	char * compute_parameter = _clear_data(parameter);
+
 	uint8_t symbol_ctnr = 0;
 	if (compute_parameter == NULL)
 		return INCORRECT_PARAMETER_INPUT;
 
+	char compared_strings [strlen(compute_parameter)];
+	strncpy(compared_strings, compute_parameter, strlen(compute_parameter));
+
+
 	switch (type_parameter){
 		case MODE_PARAMETER:
-			if (strcmp(compute_parameter, "akk1") == 0 || 
-				strcmp(compute_parameter, "akk2") == 0 ||
-			 	strcmp(compute_parameter, "gen") == 0)
-			{
+			if (strncmp(compared_strings, "akk1", 4) == 0 || 
+				strncmp(compared_strings, "akk2", 4) == 0 ||
+			 	strncmp(compared_strings, "gen", 3) == 0)
 				return SUCCESSFUL;
-			} else return UNEXISTING_CHARGER;	
+			else return UNEXISTING_CHARGER;	
 		break;
 
 		case PULSE_WIDTH_PARAMETER:
-			Extra_letters(compute_parameter, symbol_ctnr);
-			if (atoi(compute_parameter) >= LOWER_WIDTH && atoi(compute_parameter) <= TOP_WIDTH)
+			
+			if (SUCCESSFUL != extra_letters(compared_strings, symbol_ctnr))
+				return INCORRECT_PARAMETER_INPUT;
+			if (atoi(compared_strings) >= LOWER_WIDTH && atoi(compared_strings	) <= TOP_WIDTH)
 				return SUCCESSFUL;
 			
 			return INCORRECT_PULSE_WIDTH;
 		break;
 
 		case TIME_PARAMETER:
-			Extra_letters(compute_parameter, symbol_ctnr);
+			if (SUCCESSFUL != extra_letters(compute_parameter, symbol_ctnr))
+				return INCORRECT_PARAMETER_INPUT;
 
-			if (atoi(compute_parameter) >= LOWER_LIMIT_TIME && atoi(compute_parameter) <= UPPER_LIMIT_TIME)
+			if (atoi(compared_strings) >= LOWER_LIMIT_TIME && atoi(compared_strings) <= UPPER_LIMIT_TIME)
 				return SUCCESSFUL;
-			else if (atoi(compute_parameter) < LOWER_LIMIT_TIME)
+			else if (atoi(compared_strings) < LOWER_LIMIT_TIME)
 				return LESS_POSSIBLE;
-			else if (atoi(compute_parameter) > UPPER_LIMIT_TIME)
+			else if (atoi(compared_strings) > UPPER_LIMIT_TIME)
 				return MORE_POSSIBLE;
 	
 			return INCORRECT_PARAMETER_INPUT;
