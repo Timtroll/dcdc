00fab14ffc18b7eb6d1ec7fdd534d54328129e77
SergX961 <50412398+SergX961@users.noreply.github.com>
2020-11-08 18:55:39 +0300

    delete unused functions

--- software/src/measurements/measurements.c
+++ software/src/measurements/measurements.c
@@ -2,15 +2,6 @@
 #include "measurements_core.h"
 #include "charger_core.h"
 
-// char * get_for_voltage_cut_off (void) {
-// 	//return some_voltage_pointer
-// }
-
-// char * get_for_charge_current (void) {
-// 	//return some_current_pointer
-// }
-
-//=============================================
 #define MEAS_CHARGE_RESET                0
 #define MEAS_CHARGE_POSITIVE_PULSE_START 1
 #define MEAS_CHARGE_POSITIVE_PULSE_END   2
